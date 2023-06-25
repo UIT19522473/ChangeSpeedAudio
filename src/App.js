310b52c2ec8020042152bd753112a68310c4339f
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-06-25 10:54:34 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -350,7 +350,7 @@
 
 // import React from 'react'
 import React, { useState, useEffect, useRef } from "react";
-import Mp3 from "../src/medias/test1.mp3";
+// import Mp3 from "../src/medias/test1.mp3";
 import "./App.css";
 
 const App = () => {
